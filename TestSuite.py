9be3a84961615e8977da3752c05d0266160a3589
kerusey <38439184+kerusey@users.noreply.github.com>
2021-02-21 17:58:40 +0300

    initional commit

---
+++ TestSuite.py
@@ -0,0 +1,15 @@
+import getpass
+import os
+import socket
+from datetime import datetime
+from uuid import getnode as get_mac
+import pyautogui
+import speedtest
+import telebot
+import psutil
+import platform
+from PIL import Image
+
+'''
+path will be added later
+'''

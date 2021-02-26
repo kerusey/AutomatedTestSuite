48596d7c50bdb04e1eca57b278d98b021eff5a0a
kerusey <38439184+kerusey@users.noreply.github.com>
2021-02-26 19:01:19 +0300

    Removed test file, updated dependencies. Project migrated to different tgapi version

--- TestSuite.py
+++ TestSuite.py
@@ -2,26 +2,43 @@ from datetime import datetime
 import telebot
 import json
 from Tests import NetworkSpeedTest, Screenshot, UserInfoTest
+import time
 
 # config settings
 config = json.load(open("config.json"))
-app = telebot.TeleBot(__name__)
-app.config['api_key'] = config['token']
-chatID = config['userSession']
+bot = telebot.TeleBot(config['token'], parse_mode="Markdown")
+
+
+def scheduler(beginTimer):
+    animation = ['-', '\\', '|', '/', '-', '\\', '|', '/', '-']
+    animationIndex = 0
+    messageText = ("``` New job has been started at {}. Working ... {}\n" + " - [ ] {}\n" * len(config['tasks']) + "```")
+    startMessage = bot.send_message(chat_id=config['userSession'],
+                                    text=messageText.format(beginTimer, animation[animationIndex], *config['tasks']))
+    while True:
+        time.sleep(1)
+        bot.edit_message_text(chat_id=config['userSession'],
+                              message_id=startMessage.message_id,
+                              text=messageText.format(beginTimer, animation[animationIndex+1], *config['tasks']))
+        if animationIndex < len(animation) - 2:
+            animationIndex += 1
+        else:
+            animationIndex = 0
+
+
+scheduler(str(datetime.now())[:-7])
+exit()
 
 starts = datetime.now()
-
-app.send_message(chatID, "New job has been started at {}".format(datetime.now()))
-app.send_message(chatID, "-"*10)
 
 client = UserInfoTest.getUserInfo()
 network = NetworkSpeedTest.calculate()
 
 for header in client.keys():
-    app.send_message(chatID, "{} - {}".format(header, client[header]))
+    bot.send_message(chat_id=config['userSession'], text="{} - {}".format(header, client[header]))
     
 for header in network:
-    app.send_message(chatID, "{} - {}".format(header, network[header]))
+    bot.send_message(chat_id=config['userSession'], text="{} - {}".format(header, network[header]))
     
 
 '''
@@ -34,5 +51,5 @@ except Exception:
 
 ends = datetime.now()
 workSpeedTest = ends - starts
-app.send_message(chatID, "It took for {} to complete task".format(workSpeedTest))
-app.send_message(chatID, '-'*10)
+bot.send_message(chat_id=config['userSession'], text="It took for {} to complete task".format(workSpeedTest))
+bot.send_message(chat_id=config['userSession'], text='-'*10)
--- test.py
+++
@@ -1,8 +0,0 @@
-from telebot import TeleBot
-
-app = TeleBot(__name__)
-app.config['api_key'] = '1607134909:AAHQbKK4SG5ynNYjaWp8h5KJg3s8fqK1C9A'
-chat_dest = 331774665
-app.send_message(chat_dest, "")
-
-
